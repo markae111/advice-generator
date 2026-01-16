c03a53a726cebcde115ad6ecb5a97779a0a23e15
markae111 <kawedobenjamine6@gmail.com>
2026-01-16 14:24:27 +0000

    corrections

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 const container = document.querySelector(".advice-container");
-
+const adviceTxt = document.querySelector(".advice-txt");
 const dice = document.querySelector(".dice");
+const adviceId = document.querySelector(".advice-id");
 
 // using fetch to get the backend api containg the advice
 
 async function getAdvice() {
-  const response = await fetch("https://api.adviceslip.com/advice");
+  try {
+    // this api can return the same advice more than once
+    const response = await fetch("https://api.adviceslip.com/advice");
 
-  let text = await response.json();
-  console.log(text.slip.advice);
-  return {
-    adviceTxt: text.slip.advice,
-    adviceId: text.slip.id
+    // converting the data gotten from the backend to a json string
+    let text = await response.json();
+    console.log(text.slip.advice);
+
+    return {
+      adviceTxt: text.slip.advice,
+      adviceId: text.slip.id,
+    };
+  } catch (error) {
+    alert("failed to fetch data");
   }
 }
 getAdvice();
 
 dice.addEventListener("click", async () => {
+  try {
+    const advice = await getAdvice();
 
-    const user = await getAdvice()
-    if(user){
-        const adviceTxt = document.querySelector(".advice-txt");
-        adviceTxt.textContent = `"${user.adviceTxt}"`;
+    adviceTxt.textContent = `"${advice.adviceTxt}"`;
 
-        const adviceId = document.querySelector(".advice-id");
-        adviceId.textContent = `ADVICE #${user.adviceId}`;
-    }
- 
- 
+    adviceId.textContent = `ADVICE #${advice.adviceId}`;
+  } catch (error) {
+    console.log("error loading advice");
+  }
 });
